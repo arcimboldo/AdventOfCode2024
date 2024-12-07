9beec96f32fd07ec871f0948549c66bec1fa3081
Arcimboldo <arcimboldo@gmail.com>
2024-12-07 14:56:14 +0100

    Use utility app for p1

--- p1.py
+++ p1.py
@@ -1,22 +1,39 @@
+import collections
+from utils import app
 
-data  ='''
+class App(app.App):
+    def part_one(self, data, debug=False):
+        data = [i.split() for i in data.splitlines()]
+        data = [(int(i), int(j)) for i, j in data]
+        left = [i[0] for i in data]
+        right = [i[1] for i in data]
+        left.sort()
+        right.sort()
+        diff = [abs(j-i) for i, j in zip(left, right)]
+        if debug:
+            print(left, right, diff)
+        return sum(diff)
+        
+    def part_two(self, data, debug=False):
+        data = [i.split() for i in data.splitlines()]
+        data = [(int(i), int(j)) for i, j in data]
+        left = [i[0] for i in data]
+        right = [i[1] for i in data]
+        leftCounter = collections.Counter(left)
+        rightCounter = collections.Counter(right)
+
+        left = [i*rightCounter[i] for i in left]
+        return sum(left)
+
+myapp = App(
+'''
 3   4
 4   3
 2   5
 1   3
 3   9
 3   3
-'''.strip().splitlines()
+'''.strip()
+)
 
-with open('p1data.txt') as f:
-    data = f.readlines()
-
-data = [i.split() for i in data]
-data = [(int(i), int(j)) for i, j in data]
-left = [i[0] for i in data]
-right = [i[1] for i in data]
-left.sort()
-right.sort()
-diff = [abs(j-i) for i, j in zip(left, right)]
-print(left, right, diff)
-print(sum(diff) )
+myapp.run()
