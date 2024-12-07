6db5e17537d3c75fe13fc1b7805b4412f2b01e9f
Arcimboldo <arcimboldo@gmail.com>
2024-12-07 15:05:35 +0100

    Update p7 to use new app class

--- p7.py
+++ p7.py
@@ -1,39 +1,5 @@
-from utils import download
-import sys
+from utils import app
 import copy
-
-if "prod" in sys.argv:
-    data = download.read(7)
-else:
-    data = """190: 10 19
-3267: 81 40 27
-83: 17 5
-156: 15 6
-7290: 6 8 6 15
-161011: 16 10 13
-192: 17 8 14
-21037: 9 7 18 13
-292: 11 6 16 20
-"""
-
-operations = dict()
-for line in data.splitlines():
-    s, operands = line.split(":", 1)
-    operations[int(s)] = list(map(int, operands.split()))
-
-
-# def find_combinations(operands):
-#     if not operands:
-#         return [0]
-#     print(f'operands: {operands}')
-#     reminder = find_combinations(operands[1:])
-#     ops = [operands[0] + x for x in reminder] + [operands[0] * x for x in reminder]
-#     if len(operands) > 1:
-#         ops.extend(find_combinations(
-#         [int(f'{operands[1]}{operands[0]}')] + operands[2:]
-#     ))
-#     return  ops
-
 
 def find_combinations(cur, operands):
     # pick the first two and do some operation
@@ -54,12 +20,40 @@ def find_combinations2(cur, operands):
     )
 
 
-total = 0
-for s, operands in operations.items():
-    x = find_combinations2(operands[0], operands[1:])
-    # print(f"{s}: {operands}: {x}")
-    if s in x:
-        total += s
+class App(app.App):
+    def fill_operations(self, data):
+        operations = dict()
+        for line in data.splitlines():
+            s, operands = line.split(":", 1)
+            operations[int(s)] = list(map(int, operands.split()))
+        return operations
+    
+    def _runme(self, data, find_combinations_func, debug=False):
+        total = 0
+        operations = self.fill_operations(data)
+        for s, operands in operations.items():
+            x = find_combinations_func(operands[0], operands[1:])
+            if debug:
+                print(f"{s}: {operands}: {x}")
+            if s in x:
+                total += s
+        return total
+    
+    def part_one(self, data, debug=False):
+        return self._runme(data, find_combinations, debug)
+    
+    def part_two(self, data, debug=False):
+        return self._runme(data, find_combinations2, debug)
+        
 
-# test must be 3749
-print(f"Part one: {total}")
+myapp = App("""190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+""")
+myapp.run()
