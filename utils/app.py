544fd0bd6d06d26d318f2a9b2c8fc10834881c62
Arcimboldo <arcimboldo@gmail.com>
2024-12-11 10:13:58 +0100

    Add attribute "prod" to check if this is prod or test run and print it in the output

--- utils/app.py
+++ utils/app.py
@@ -11,6 +11,7 @@ class App:
         self.day = day if day is not None else int(reday.match(sys.argv[0]).group(1))
         self._data = download.read(self.day)
         self.debug = '-d' in sys.argv
+        self.prod = 'prod' in map(lambda x: x.lower(), sys.argv)
 
     def log(self, *args, **kw):
         if self.debug:
@@ -24,9 +25,9 @@ class App:
     
     @property
     def data(self):
-        return self._data if 'prod' in sys.argv else self._testdata
+        return self._data if self.prod else self._testdata
         
     def run(self):
-        print(f'Day {self.day}')
+        print(f'Day {self.day} {"PROD" if self.prod else "TEST"}')
         print(f'  part one: {self.part_one()}')
         print(f'  part two: {self.part_two()}')
