46de1191e4abb8b05a9356de27e178bc38226822
Arcimboldo <arcimboldo@gmail.com>
2024-12-18 13:54:25 +0100

    Add methods test_one and test_two

--- utils/app.py
+++ utils/app.py
@@ -31,3 +31,22 @@ class App:
         print(f'Day {self.day} {"PROD" if self.prod else "TEST"}')
         print(f"  part one: {self.part_one()}")
         print(f"  part two: {self.part_two()}")
+
+    def _test(self, method, want_test, want_prod):
+        oldprod = self.prod
+        self.prod = False
+        got_test = method()
+        if got_test != want_test:
+            print(f'ERROR: TEST run of {method.__name__}: got {got_test}, expected {want_test} instead')
+        self.prod = True
+        got_prod = method()
+        if got_prod != want_prod:
+            print(f'ERROR: TEST run of {method.__name__}: got {got_test}, expected {want_prod} instead')
+        self.prod = oldprod
+        return got_test == want_test and got_prod == want_prod
+
+    def test_one(self, *args, **kw):
+        return self._test(self.part_one, *args, **kw)
+
+    def test_two(self, *args, **kw):
+        return self._test(self.part_two, *args, **kw)
