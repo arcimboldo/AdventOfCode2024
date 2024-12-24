88c0fe5d2b35055f13f6b38708613d4d888803bb
Arcimboldo <arcimboldo@gmail.com>
2024-12-24 14:33:58 +0100

    Add solution for part two, day 17

--- p17.py
+++ p17.py
@@ -1,5 +1,5 @@
 from utils import app
-import re
+from collections import defaultdict, deque
 
 
 class State:
@@ -10,6 +10,7 @@ class State:
         self.program = program
         self.ptr = 0
         self.output = []
+        self.options_for = defaultdict(list)
 
     def next(self):
         """Run the current instruction"""
@@ -28,9 +29,6 @@ class State:
         op = self.program[self.ptr + 1]
 
         ret = func(op, self)
-        # print(
-        #     f"Running function opcode {opcode} at ptr {self.ptr}: {func.__name__}({op}) -> {ret}"
-        # )
         if ret is not None:
             self.ptr = ret
         else:
@@ -38,7 +36,15 @@ class State:
 
 
 def combo(op, state):
-    return {0: 0, 1: 1, 2: 2, 3: 3, 4: state.A, 5: state.B, 6: state.C}[op]
+    return {
+        0: 0,
+        1: 1,
+        2: 2,
+        3: 3,
+        4: state.A,
+        5: state.B,
+        6: state.C,
+    }[op]
 
 
 def adv(op, state):
@@ -47,7 +53,7 @@ def adv(op, state):
     of the instruction's combo operand. (So, an operand of 2 would divide A by 4
     (2^2); an operand of 5 would divide A by 2^B.) The result of the division
     operation is truncated to an integer and then written to the A register."""
-    state.A = state.A // 2 ** combo(op, state)
+    state.A = state.A >> combo(op, state)
 
 
 def bxl(op, state):
@@ -84,21 +90,22 @@ def out(op, state):
     """The out instruction (opcode 5) calculates the value of its combo operand
     modulo 8, then outputs that value. (If a program outputs multiple values,
     they are separated by commas.)"""
-    state.output.append(str(combo(op, state) % 8))
+    got = combo(op, state) % 8
+    state.output.append(str(got))
 
 
 def bdv(op, state):
     """The bdv instruction (opcode 6) works exactly like the adv instruction
     except that the result is stored in the B register. (The numerator is still
     read from the A register.)"""
-    state.B = state.A // 2 ** combo(op, state)
+    state.B = state.A >> combo(op, state)
 
 
 def cdv(op, state):
     """The cdv instruction (opcode 7) works exactly like the adv instruction
     except that the result is stored in the C register. (The numerator is still
     read from the A register.)"""
-    state.C = state.A // 2 ** combo(op, state)
+    state.C = state.A >> combo(op, state)
 
 
 def parse(txt):
@@ -111,17 +118,58 @@ def parse(txt):
 
 
 class App(app.App):
+
     def part_one(self):
+        print(self.data)
         state = State(*parse(self.data))
 
+        self.log(f"Program: {state.program}")
         while state.ptr < len(state.program):
             state.next()
         output = str.join(",", state.output)
-        print(f'Output: {output}')
+        # print(f"Output: {output}")
         return output
 
     def part_two(self):
-        pass
+        state = State(*parse(self.data))
+        A = 0
+        print(f"Program: {state.program}")
+        queue = deque()
+        j = len(state.program) - 1
+        queue.append((0, j))
+
+        while queue and j >= 0:
+            a, j = queue.popleft()
+            want = state.program[j]
+            if j == -1:
+                # Got the result
+                A = a
+                break
+            a <<= 3
+            for i in range(8):
+                # Run until the jump instruction
+                state.A = a + i
+                state.ptr = 0
+                while state.program[state.ptr] != 3:
+                    if state.program[state.ptr] == 0:
+                        state.ptr += 2
+                    state.next()
+                if int(state.output[-1]) == want:
+                    # print(
+                        # f"  FOUND that want={want} can be found if if A is {a}+{i} = {A} ({A:0b})"
+                    # )
+                    queue.append((state.A, j - 1))
+            # print(f'queue: {queue}')
+        state.A = A
+        state.output = []
+        while state.ptr < len(state.program):
+            state.next()
+        output = str.join(",", state.output)
+        print(f"Output: {output}")
+        if output == str.join(",", map(str, state.program)):
+            print("GOT IT")
+
+        return A
 
 
 myapp = App(
@@ -134,6 +182,28 @@ Program: 0,1,5,4,3,0
 """
 )
 
-myapp.test_one('4,6,3,5,6,3,5,2,1,0', '5,0,3,5,7,6,1,5,4')
+myapp.test_one("4,6,3,5,6,3,5,2,1,0", "5,0,3,5,7,6,1,5,4")
 
-myapp.run()
+#     Register A 117440
+myapp = App(
+    f"""Register A: { 8*8*(3+8*(5+8*(4+8*(3))))}
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0
+"""
+)
+myapp.test_one("0,3,5,4,3,0", None)
+
+myapp = App(
+    """
+            # Register A: 1
+# Register B: 0
+# Register C: 0
+
+# Program: 2,4,1,1,7,5,1,5,0,3,4,4,5,5,3,0
+"""
+)
+myapp.test_two(None, 164516454365621)
+# myapp.run()
+print(f"Running part two: {myapp.part_two()}")
