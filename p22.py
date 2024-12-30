5d342ddaf240c787d56798df574e1df109b09c7a
Arcimboldo <arcimboldo@gmail.com>
2024-12-30 18:41:41 +0100

    day 22 part one easy

--- p22.py
+++ p22.py
@@ -1,5 +1,5 @@
 from utils import app
-
+from collections import defaultdict
 
 def calculate(s, n ):
     for i in range(n):
@@ -9,6 +9,7 @@ def calculate(s, n ):
 
         s = ((s << 11) ^ s) % 16777216
         yield s
+
 
 class App(app.App):
     def part_one(self):
@@ -21,7 +22,30 @@ class App(app.App):
         return s
 
     def part_two(self):
-        pass
+        numbers = list(map(int, self.data.splitlines()))
+        prices = {}
+        for n in numbers:
+            prev = 0
+            changes = []
+            for last in calculate(n, 2000):
+                d = last%10
+                changes.append(d-prev)
+                prev = d
+            prices[n] = changes
+        slices = defaultdict(dict)
+        for n, p in prices.items():
+            for i in range(0, 2000):
+                s = p[i:i+4]
+                if len(s) != 4:
+                    break
+                t = tuple(p[i:i+4]) 
+                slices[t][n] = max(slices[t].get(n, 0), s[i+3])
+        best = {}
+        for t in slices:
+            s = sum(t.values())
+            best[t] = s
+        m = max(best.values())
+        return [t for t in best if best[t] == m][0]
 
 
 myapp = App("""
@@ -30,4 +54,11 @@ myapp = App("""
 100
 2024
 """)
-myapp.run()
+myapp.test_one(None, 20411980517)
+myapp = App('''1
+2
+3
+2024
+''')
+myapp.test_two([-2,1,-1,3], None)
+# myapp.run()
