2127444d3fe9411f3704c6d59e2f12f59cf0d345
Arcimboldo <arcimboldo@gmail.com>
2025-01-01 12:27:50 +0100

    Day 23 Part two working

--- p23.py
+++ p23.py
@@ -1,16 +1,16 @@
 from pprint import pprint
 from utils import app
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 class App(app.App):
     def part_one(self):
-        graph = defaultdict(list)
+        graph = defaultdict(set)
         for line in self.data.splitlines():
             a, b = line.split('-')
-            graph[a].append(b)
-            graph[b].append(a)
-        print(graph)
+            graph[a].add(b)
+            graph[b].add(a)
+
         valid = set()
         for node, nodes in graph.items():
             if not node.startswith('t'):
@@ -29,7 +29,38 @@ class App(app.App):
         return len(valid)
 
     def part_two(self):
-        pass
+        graph = defaultdict(set)
+        for line in self.data.splitlines():
+            a, b = line.split('-')
+            graph[a].add(b)
+            graph[b].add(a)
+        # Sane as part one but without limit
+        max_loops = set()
+        for node in graph:
+            # This is the start of the loop
+            loop = set()
+            loop.add(node)
+            q = deque(graph[node])
+            while q:
+                n = q.popleft()
+
+                if n in loop:
+                    continue
+                # Find if this is a clique. graph[n] needs to have connections to all the nodes in the loop
+                # In set term, it means that the loop needs to be a subset of all the visited nodes
+                if loop.issubset(graph[n]):
+                    loop.add(n)
+            # We should have found the biggest clique for 'node'
+            sloop = tuple(sorted(loop))
+
+            max_loops.add(sloop)
+        
+        max_len, max_loop = 0, None
+        for loop in max_loops:
+            if len(loop) > max_len:
+                max_len = len(loop)
+                max_loop = loop
+        return str.join(',', max_loop)
         
 myapp = App('''kh-tc
 qp-kh
